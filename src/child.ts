6850a63a4e8b64949b01e99a1e9e2435d92d1511
caiogmrocha <caioguilherme739@gmail.com>
2024-03-16 13:06:48 -0300

    feat: ensure that the videos will be stored as .m3u8 and .ts

--- src/child.ts
+++ src/child.ts
@@ -1,20 +1,22 @@
 import path from 'node:path';
 import crypto from 'node:crypto';
+import fs from 'node:fs';
 
 import ffmpeg from 'fluent-ffmpeg';
 
 const resolutions = [
   '320x240',
-  '640x360',
-  '640x480',
-  '1280x720',
-  '1920x1080'
-]
+  // '640x360',
+  // '640x480',
+  // '1280x720',
+  // '1920x1080'
+];
 
 let c = 0;
 
 let handler = ffmpeg({
-  source: process.stdin
+  source: process.stdin,
+  timeout: 1000 * 60 * 7
 })
 .on('error', console.error)
 .on('end', () => {
@@ -27,8 +29,18 @@ let handler = ffmpeg({
 const fileUUID = crypto.randomUUID();
 
 resolutions.forEach((resolution, index) => {
+  fs.mkdirSync(path.resolve(__dirname, '..', 'uploads', `sliced-videos`, fileUUID));
+
   handler = handler
-    .output(path.resolve(__dirname, '..', 'uploads', `${fileUUID}@${resolution}.mp4`))
+    .output(path.resolve(__dirname, '..', 'uploads', `sliced-videos`, fileUUID, `${resolution}.m3u8`))
+    .addOptions([
+      '-profile:v baseline',
+      '-level 3.0',
+      '-start_number 0',
+      '-hls_time 10',
+      '-hls_list_size 0',
+      '-f hls'
+    ])
     .videoCodec('libx264')
     .size(resolution)
 });
